88311c220ba4b23fe51f337637484574f93b97e4
JunObata <s1520627@u.tsukuba.ac.jp>
2015-07-04 11:30:30 +0900

    spec3

--- src/test/Cooking.java
+++ src/test/Cooking.java
@@ -1,7 +1,31 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Cooking {
 	public static void main(String[] args){
-		System.out.println("オムライス");
+		/*
+		 * ディレクトリ構造が変わったらfilePassを変更する必要あり
+		 */
+		final String filePass = "C:/Users/jun/pleiades/workspace/test/recipe-data.txt";
+		try{
+			/*
+			 * recipe-dataをオープン
+			 * ユーザの階層へのパス書き換えが必要
+			 *
+			 */
+			BufferedReader br = new BufferedReader(new FileReader(filePass));
+
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+
+			br.close();
+			}catch(IOException e){
+			  System.out.println(e + "ファイル操作エラー");
+			}
 		}
 }
