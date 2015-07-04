fb330f5c43e819ee72c15de1fe92ff3b6f4ae566
JunObata <s1520627@u.tsukuba.ac.jp>
2015-07-04 12:02:58 +0900

    spec5

--- src/test/Cooking.java
+++ src/test/Cooking.java
@@ -7,22 +7,21 @@ import java.io.IOException;
 public class Cooking {
 	public static void main(String[] args){
 		/*
-		 * ディレクトリ構造が変わったらfilePassを変更する必要あり
+		 * ディレクトリ構造が変わったらrecipe-data.txt
+		 * へのfilePassを変更する必要あり
 		 */
 		final String filePass = "C:/Users/jun/pleiades/workspace/test/recipe-data.txt";
+		
 		try{
-			/*
-			 * recipe-dataをオープン
-			 * ユーザの階層へのパス書き換えが必要
-			 *
-			 */
 			BufferedReader br = new BufferedReader(new FileReader(filePass));
 
-			String line = "";
-			while ((line = br.readLine()) != null) {
-				System.out.println(line);
+			String outputLine = "";
+			/*一行ずつ出力*/
+			int id = 1;
+			while ((outputLine = br.readLine()) != null) {
+				System.out.println(id + ": " + outputLine);
+				id++;
 			}
-
 			br.close();
 			}catch(IOException e){
 			  System.out.println(e + "ファイル操作エラー");
